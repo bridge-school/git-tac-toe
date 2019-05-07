47584f1eeb1ada9293b8c2439f526147fdd767b2
Corina Wong <hello@corinawong.com>
2019-05-07 18:47:13 -0400

    Corina's move 2

--- src/yourMoves.js
+++ src/yourMoves.js
@@ -1,9 +1,6 @@
 export const x = '❌';
 export const o = '⭕️';
 
-const moves = [
-    [[x], [], []], 
-    [[], [o], []], 
-    [[], [], [x]]];
+const moves = [[[x], [], []], [[o], [o], []], [[], [], [x]]];
 
 export default moves;
